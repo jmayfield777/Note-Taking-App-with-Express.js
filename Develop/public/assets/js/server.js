535b28e1327359abfaa68d6af54e8d3312ef3f7b
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-04 15:34:25 -0500

    added get route to notes.html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -11,3 +11,8 @@ const app = express();
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
+
+// route to note.html file
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
