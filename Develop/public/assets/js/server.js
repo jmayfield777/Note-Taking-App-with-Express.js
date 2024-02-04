7b48fbeef4f7826fe3fcb2af7fc4e7dff493d1fd
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-04 16:30:51 -0500

    added get route for /api/notes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 // import uuid
 const { v4: uuidv4 } = require('uuid');
 
+// import db/db.json
+const notes = require('../../../db/db.json');
+
 // set port
 const PORT = 3001;
 
@@ -13,12 +16,17 @@ const fs = require('fs');
 // assign instance of express to a variable
 const app = express();
 
-// route to index.html file
+// get route to index.html file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+  res.sendFile(path.join(__dirname, '../../../public/index.html'));
 });
 
-// route to note.html file
+// get route to notes.html file
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+  res.sendFile(path.join(__dirname, '../../../public/notes.html'));
+});
+
+// get route to /api/notes
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
