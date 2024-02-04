492deedc97c43e88f67fb1a92e4f5a8c775c0fc2
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-04 17:24:31 -0500

    added post route to /api/notes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -18,15 +18,76 @@ const app = express();
 
 // get route to index.html file
 app.get('*', (req, res) => {
+
   res.sendFile(path.join(__dirname, '../../../public/index.html'));
+
 });
 
 // get route to notes.html file
 app.get('/notes', (req, res) => {
+
   res.sendFile(path.join(__dirname, '../../../public/notes.html'));
+
 });
 
 // get route to /api/notes
 app.get('/api/notes', (req, res) => {
+  // parse json data
   res.json(notes);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+// post route to /api/notes
+app.post('/api/notes', (req, res) => {
+
+// log that a POST request was received to the terminal
+console.info(`${req.method} request received to add a note`);
+  
+// destructuring assignment for items in req.body
+const { title , text } = req.body;
+
+// if all required properties are present
+if (title && text) {
+// variable for the newNote object
+const newNote =  {
+  title,
+  text,
+  id: uuidv4()
+};
+
+// get existing notes
+fs.readFile('../../../db/db.json', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    // convert string to json object
+    const parsedNotes = JSON.parse(data);
+
+    // add a new note
+    parsedNotes.push(newNote);
+
+    // write updated notes back to db.json
+    fs.writeFile(
+      '../../../db/db.json',
+      JSON.stringify(parsedNotes, null, 4),
+      (writeErr) => 
+        writeErr
+          ? console.error(writeErr)
+          : console.info('Successfully updated notes!')
+    );
+  }
+});
+
+const response = {
+  status: 'success',
+  body: newNote,
+};
+
+  console.log(response);
+  res.status(201).json(response);
+} else {
+  res.status(500).json('Error in posting note');
+}
 });
