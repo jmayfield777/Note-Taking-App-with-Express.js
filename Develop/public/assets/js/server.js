836be9f8d239390418a18db2056d1611eaa337a9
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-04 15:27:08 -0500

    added get route to index.html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -6,3 +6,8 @@ const PORT = 3001;
 
 // assign instance of express to a variable
 const app = express();
+
+// route to index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
