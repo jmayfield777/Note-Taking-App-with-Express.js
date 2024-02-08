d2dcf253ce3e0e374abe32883e207eca31ca91c9
Bacala <bacala@Bacalas-MacBook-Pro.local>
2024-02-08 14:28:12 -0800

    set port and middleware for body parsing

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,10 @@ const express = require('express');
 // create instance of express and assign it to variable
 const app = express();
 
+// PORT
+const PORT = process.env.PORT || 3001;
+
+// middleware for body parsing
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded( { extended: true }));
