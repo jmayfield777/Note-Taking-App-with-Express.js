addee0ea1d16f32a8eb10036b54b3de948ae3a7e
Bacala <bacala@Bacalas-MacBook-Pro.local>
2024-02-08 14:32:58 -0800

    imported html and api routes and set port to listen at 3001

--- Develop/server.js
+++ Develop/server.js
@@ -11,3 +11,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded( { extended: true }));
+
+// import html and api routes
+const apiRoutes = require('./routes/apiRoutes.js');
+app.use(apiRoutes);
+const htmlRoutes = require('./routes/htmlRoutes.js');
+app.use(htmlRoutes);
+
+// listen to port
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
